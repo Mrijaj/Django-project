7854ddf55bb9acdfa30e947502db16339a1f8c4b
MRIJAZ404 <ijaz04186@gmail.com>
2025-01-20 23:27:08 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Scribble Game
+Noun1=input("Enter a naming word (Noun): ")
+Verb1=input("Enter a verb with has action or state: ")
+adverb1=input("Enter a adverb which states a verb or adjective: ")
+adjective1=input("Enter a word that adds more information about place, time, manner: ")
+Pronoun1=input("Enter a pronoun that replaces a noun: ")
+print(f"{Noun1} is a {Verb1} guy and is always {adverb1} at {adjective1} to people {Pronoun1}")
